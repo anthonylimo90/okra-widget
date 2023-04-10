91644eb9130da14b138cf809aff72449b0513ca1
Anthony Kiplimo <130223066+anthonylimo90@users.noreply.github.com>
2023-04-10 13:44:04 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
+import Okra from 'okra-js';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const widgetOkra = () => {
+    Okra.buildWithOptions({
+      name: 'TestingTutortial',
+      env: 'production-sandbox',
+      key: '0191f18f-647e-5cf9-b63b-6be4ce8e1da9',
+      token: '5da6358130a943486f33dced', 
+      source: 'link',
+      color:  '#009E74',
+      limit: '24',
+      callback_url: '',
+      redirect_url: '',
+      currency: 'NGN',
+      logo: 'https://dash.okra.ng/static/media/OkraFullLogo.4bc5f093.png',
+      onSuccess: function(data){
+          console.log('success', data)
+      },
+      onClose: function(){
+          console.log('closed')
+      }
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Limo's App</p>
+        <button onClick={() => widgetOkra()}>Connect with Okra</button>
       </header>
     </div>
   );
